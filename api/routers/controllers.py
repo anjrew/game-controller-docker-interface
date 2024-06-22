63bc9aa77abf655165773e919780c2fcdbe0119f
Andrew Johnson <earyzhe@gmail.com>
2024-06-22 11:15:34 +0200

    Fix controller router

--- api/routers/controllers.py
+++ api/routers/controllers.py
@@ -1,10 +1,11 @@
 import logging
-from typing import List
+from typing import List, Optional
 
 from dependency_injector.wiring import Provide
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from api.dependencies import DependencyContainer
+from game_controllers.models.controller_state import ControllerState
 from game_controllers.models.joystick_details import JoystickDetails
 from game_controllers.services.joystick_service import JoystickService
 
@@ -26,13 +27,15 @@ async def list_controllers(
 @router.post("/controller/{joystick_id}/initialize")
 async def initialize_controller(
     joystick_id: int,
+    controller_type: str,
+    platform: Optional[str] = None,
     joystick_service: JoystickService = Depends(
         Provide[DependencyContainer.joystick_service]
     ),
 ) -> str:
     LOGGER.info(f"Initialize joystick {joystick_id}")
     try:
-        joystick_service.ini(joystick_id)
+        joystick_service.create_joystick(joystick_id, controller_type, platform)
         return f"Joystick {joystick_id} initialized"
     except Exception as e:
         LOGGER.error(f"Failed to initialize joystick {joystick_id}: {e}")
@@ -45,15 +48,24 @@ async def get_controller_state(
     joystick_service: JoystickService = Depends(
         Provide[DependencyContainer.joystick_service]
     ),
-) -> Dict[str, Any]:
+) -> ControllerState:
     LOGGER.info(f"Get state of joystick {joystick_id}")
     try:
         return joystick_service.get_joystick_state(joystick_id)
+    except KeyError as e:
+        LOGGER.error(
+            f"Failed to find joystick with id{joystick_id}: {e}", exc_info=True
+        )
+        raise HTTPException(status_code=404, detail=str(e))
     except ValueError as e:
-        LOGGER.error(f"Failed to get state of joystick {joystick_id}: {e}")
+        LOGGER.error(
+            f"Failed to get value of joystick {joystick_id}: {e}", exc_info=True
+        )
         raise HTTPException(status_code=404, detail=str(e))
     except Exception as e:
-        LOGGER.error(f"Failed to get state of joystick {joystick_id}: {e}")
+        LOGGER.error(
+            f"Failed to get state of joystick {joystick_id}: {e}", exc_info=True
+        )
         raise HTTPException(status_code=500, detail="Failed to get joystick state")
 
 
