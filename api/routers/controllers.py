28ce5509ba0fcfebca49119169655b3a49d6e981
Andrew Johnson <earyzhe@gmail.com>
2024-06-23 08:02:05 +0200

    Make type fixes

--- api/routers/controllers.py
+++ api/routers/controllers.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Dict, List, Optional
+from typing import Dict, List
 
 from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends, HTTPException
